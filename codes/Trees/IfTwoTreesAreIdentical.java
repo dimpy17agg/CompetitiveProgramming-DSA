c558ed383a7cf21e51cd3a1965ed9bf3db4a2b56
Dimpy Aggarwal <47886241+dimpy17agg@users.noreply.github.com>
2020-09-28 12:59:37 +0530

    Create IfTwoTreesAreIdentical.java

---
+++ codes/Trees/IfTwoTreesAreIdentical.java
@@ -0,0 +1,19 @@
+class Tree
+{
+	boolean isIdentical(Node root1, Node root2)
+	{
+	    // Code Here
+        if(root1==null && root2==null)
+        return true;
+        if(root1==null && root2!=null)
+        return false;
+        if(root2==null && root1!=null)
+        return false;
+        
+        if(root1.data!=root2.data)
+        return false;
+        
+        return isIdentical(root1.left,root2.left) && isIdentical(root1.right,root2.right);
+	}
+	
+}
